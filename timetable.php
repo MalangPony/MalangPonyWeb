63e767cb9cb0ee091cd9893abbc2ae71d8e16af0
SheepPony <sheepponymlp@gmail.com>
2024-10-24 23:19:40 +0900

    Mildly improve timetable styling

--- timetable.php
+++ timetable.php
@@ -10,21 +10,23 @@
 	
 	
 	<table class="timetable">
+		<!-- kinda self evident
 		<tr>
 			<th>시간</th>
 			<th>활동</th>
 			<th>설명</th>
 		</tr>
+		-->
 		<tr class="tt_row">
-			<td class="tt_time">10:00</td>
+			<td class="tt_time" style="height:0;">10:00</td>
 			<td class=""></td>
 			<td class="tt_expl"></td>
 		</tr>
 		<tr class="tt_row">
 			<td class="tt_time">10:30</td>
 			<td class="tt_block tt_block_red" rowspan="2">
-				<div class="tt_trange">10:00 -</div>
-				<div class="tt_actname">참가자 입장 시작</div>
+				<div class="tt_trange">10:00 - 11:00</div>
+				<div class="tt_actname">참가자 입장</div>
 			</td>
 			<td class="tt_expl" rowspan="2">10시~11시 아무때나 입장해 주시면 됩니다.</td>
 		</tr>
@@ -43,9 +45,9 @@
 			<td class="tt_time">12:00</td>
 			<td class="tt_block tt_block_blue" rowspan="3">
 				<div class="tt_trange">11:30 - 13:00</div>
-				<div class="tt_actname">우리말 겨루기!</div>
+				<div class="tt_actname">우리말 겨루기</div>
 			</td>
-			<td class="tt_expl" rowspan="3">마이리틀포니 지식 퀴즈쇼</td>
+			<td class="tt_expl" rowspan="3">마이리틀포니 지식 퀴즈쇼!</td>
 		</tr>
 		<tr class="tt_row">
 			<td class="tt_time">12:30</td>
@@ -74,7 +76,7 @@
 				<div class="tt_trange">14:30 - 15:00</div>
 				<div class="tt_actname">보드게임 설명회</div>
 			</td>
-			<td class="tt_expl">막장 포니 커플링 보드게임,<br>TSSSF 들어보셨나요?<br>친절히 플레이 방법을 설명해 드립니다!</td>
+			<td class="tt_expl">막장 포니 커플링 보드게임, TSSSF의<br>플레이 방법을 설명해 드립니다!</td>
 		</tr>
 		<tr class="tt_row">
 			<td class="tt_time">15:30</td>
@@ -99,7 +101,7 @@
 				<div class="tt_trange">17:00 - 17:30</div>
 				<div class="tt_actname">래플 추첨 및 폐회식</div>
 			</td>
-			<td class="tt_expl">아쉬운 폐회식...그리고 두근두근 래플 추첨!</td>
+			<td class="tt_expl">아쉬운 폐회식...<br>그리고 두근두근 래플 추첨!</td>
 		</tr>
 	</table>
 	
