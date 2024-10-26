a87cf0bed3904fe8fff5d0452f83d660eb2a7439
SheepPony <sheepponymlp@gmail.com>
2024-10-27 04:20:38 +0900

    Add more bg images

--- footer.php
+++ footer.php
@@ -3,14 +3,19 @@
 <?php endif; ?>
 </div> <!-- End of Content -->
 <div class="bgdisp">
-	<?php if (rand()%2==0): ?>
+	<?php $roll=rand()%3; ?>
+	<?php if ($roll==0): ?>
 		<img src="images/KMR_CloseCrop-SizEq_1000px.png" />
 		<div></div>
 		<img src="images/HMR_CloseCrop_1000px.png" />
-	<?php else: ?>
+	<?php elseif ($roll==1): ?>
 		<img src="images/CM_CropA_L_1000px.png" style="align-self:flex-start;"/>
 		<div></div>
 		<img src="images/CM_CropA_R_1000px.png" style="align-self:flex-end;"/>
+	<?php else: ?>
+		<img src="images/detail_hanmari_rot180.png" style="align-self:flex-start;"/>
+		<div></div>
+		<img src="images/interrupter_hanmari1_NoBG.png" style="align-self:flex-end;"/>
 	<?php endif; ?>
 </div>
 </body>
