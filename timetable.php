8e50d40122e357a217790911cb93dc0dd94e31c8
EightonEight <fbud818@naver.com>
2024-10-28 01:58:16 +0900

    Update texts

--- timetable.php
+++ timetable.php
@@ -27,7 +27,7 @@
 				<div class="tt_trange">10:00 - 11:00</div>
 				<div class="tt_actname">참가자 입장</div>
 			</td>
-			<td class="tt_expl" rowspan="4">10시~11시 아무때나 입장해 주시면 됩니다.</td>
+			<td class="tt_expl" rowspan="4"></td>
 		</tr>
 		<tr >
 			<td class="tt_time" rowspan="2">10:30</td>
@@ -49,9 +49,9 @@
 		<tr >
 			<td class="tt_block tt_block_fs" rowspan="6">
 				<div class="tt_trange">11:30 - 13:00</div>
-				<div class="tt_actname">우리말 겨루기</div>
+				<div class="tt_actname">우리'말' 겨루기</div>
 			</td>
-			<td class="tt_expl" rowspan="6">마이리틀포니 지식 퀴즈쇼!</td>
+			<td class="tt_expl" rowspan="6">팀으로 진행되는 마이리틀포니 퀴즈쇼! <br>포니를 얼마나 알고 있는지 확인해보세요. <br>1등과 2등에게는 상품이 수여됩니다!</td>
 		</tr>
 		<tr >
 			<td class="tt_time" rowspan="2">12:00</td>
@@ -69,7 +69,7 @@
 				<div class="tt_trange">13:00 - 14:30</div>
 				<div class="tt_actname">드로잉 쇼 &amp; 다과회</div>
 			</td>
-			<td class="tt_expl" rowspan="6">아티스트들의 그림 실력을 구경하면서<br>맛있는 과자를 나눠먹어요!</td>
+			<td class="tt_expl" rowspan="6">간식과 함께하는 아티스트 초청 드로잉쇼!</td>
 		</tr>
 		<tr >
 			<td class="tt_time" rowspan="2">13:30</td>
@@ -87,7 +87,7 @@
 				<div class="tt_trange" rowspan="2">14:30 - 15:00</div>
 				<div class="tt_actname" rowspan="2">보드게임 설명회</div>
 			</td>
-			<td class="tt_expl" rowspan="2">막장 포니 커플링 보드게임, TSSSF의<br>플레이 방법을 설명해 드립니다!</td>
+			<td class="tt_expl" rowspan="2">막장 포니 커플링 보드게임, <br>TSSSF의 플레이 방법을 설명해 드립니다!</td>
 		</tr>
 		<tr >
 			<td class="tt_time" rowspan="2">15:00</td>
@@ -95,9 +95,9 @@
 		<tr >
 			<td class="tt_block tt_block_ts" rowspan="8">
 				<div class="tt_trange">15:00 - 17:00</div>
-				<div class="tt_actname">게임 및 자유시간</div>
+				<div class="tt_actname">보드게임 &amp; 자유시간</div>
 			</td>
-			<td class="tt_expl" rowspan="8"></td>
+			<td class="tt_expl" rowspan="8">포니 보드게임을 하며 친목을 다져보세요!</td>
 		</tr>
 		<tr >
 			<td class="tt_time" rowspan="2">15:30</td>
@@ -117,9 +117,9 @@
 		<tr >
 			<td class="tt_block tt_block_sp" rowspan="2">
 				<div class="tt_trange">17:00 - 17:30</div>
-				<div class="tt_actname">래플 추첨 및 폐회식</div>
+				<div class="tt_actname">래플 추첨 &amp; 폐회식</div>
 			</td>
-			<td class="tt_expl" rowspan="2">아쉬운 폐회식...<br>그리고 두근두근 래플 추첨!</td>
+			<td class="tt_expl" rowspan="2">행사 마지막 추첨을 통해 <br>경품을 얻을 수 있는 기회가 주어집니다!!</td>
 		</tr>
 		<tr >
 			<td class="tt_time" rowspan="2">17:30</td>
