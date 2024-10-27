dde96ffa9a70d5c5c352ece22155c93417406cd4
SheepPony <sheepponymlp@gmail.com>
2024-10-27 11:26:36 +0900

    Countdown on main page

--- index.php
+++ index.php
@@ -6,6 +6,19 @@
 <?php include 'header.php'; ?>
 	
 	
+	<p id="countdown" style="font-size:24px;"> </p>
+	<script>
+		//let eventTime=new Date("2025-02-15T12:00:00+09:00");
+		let eventTime=new Date("2025-02-15T00:00:00+09:00");
+		let nowTime=new Date();
+		let timeDelta = eventTime.getTime() - nowTime.getTime();
+		if (timeDelta<0) timeDelta=0;
+		let seconds=timeDelta/1000;
+		let days = Math.ceil(timeDelta/1000/60/60/24);		
+		document.getElementById("countdown").innerHTML = "D-"+days;
+	</script>
+	
+	
 	<p class="introtext">
 		한국의 마이리틀포니 팬 교류회, 2025년 2월 15일 서울에서 만나요!<br>
 		포니를 사랑하는 모든 분들을 환영합니다!
