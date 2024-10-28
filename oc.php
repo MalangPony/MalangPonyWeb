ae2610b8764daa13ee9b29439602ccb995ece41e
SheepPony <sheepponymlp@gmail.com>
2024-10-28 14:22:35 +0900

    Improve oc page

--- oc.php
+++ oc.php
@@ -10,6 +10,31 @@
 	
 	<a href="images/KimmariREF_Full.png"><img src="images/KimmariREF_Rsz40p-JPG90.jpg" style="max-width:100%;filter:drop-shadow(3px 3px 5px #00000080);"/></a>
 	
+	<div class="normaltext" style="display:flex;justify-content:space-evenly;margin-top:-12px;">
+		<a class="actual_link link_with_icon" href="images/KimmariREF_Full.png" download>
+			레퍼런스 시트
+			<img src="sprites/download_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24_FILLDP.png"
+				width="20px" height="20px">
+		</a>
+		<a class="actual_link link_with_icon" href="images/Hanmari_CM_rev1_3000px.png" download>
+			큐티마크(PNG)
+			<img src="sprites/download_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24_FILLDP.png"
+				width="20px" height="20px">
+		</a>
+		<a class="actual_link link_with_icon" href="images/hanmari_CM_rev1_TRIM.svg" download>
+			큐티마크(SVG)
+			<img src="sprites/download_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24_FILLDP.png"
+				width="20px" height="20px">
+		</a>
+	</div>
+	
+	<p class="normaltext">
+		<span class="placeholder">TODO: 여기 한마리 소개 텍스트 좀 자연스럽게</span><br>
+		<span class="hanmari mari_big">한마리</span>는 아이돌로 활동중인 발랄 명량 여자아이입니다.<br>
+		<span class="kimmari mari_big">김마리</span>는 <span class="hanmari">한마리</span>의 본명으로, 아이돌 활동중이 아닐 때의 평범한 고등학생의 모습입니다.<br>
+		<em>둘은 같은 인물입니다!</em>
+	</p>
+	
 	<p class="normaltext">
 		<span class="hanmari">한마리</span> 또는 <span class="kimmari">김마리</span> 팬아트는 언제든 환영입니다!<br>
 		다만 수위가 높은 그림은 자제해 주세요.<br>
