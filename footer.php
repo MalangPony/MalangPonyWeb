e3624dea7ebb34b86bd2851e273d7911c9f1d626
SheepPony <sheepponymlp@gmail.com>
2024-11-06 00:51:21 +0900

    Update background images

--- footer.php
+++ footer.php
@@ -15,38 +15,54 @@
 <?php endif; ?>
 
 <div class="bgdisp">
-	<?php $roll=rand()%4; ?>
+	<?php $roll=rand()%6; ?>
 	<?php if ($roll==0): ?>
-		<div class="bg_container bg_btm bg_left">
-			<img src="images/KMR_CloseCrop-SizEq_1000px.png" />
-		</div>
-		<div class="bg_spacer">Art by SheepPony</div>
-		<div class="bg_container bg_btm bg_right">
-			<img src="images/HMR_CloseCrop_1000px.png" />
-		</div>
-	<?php elseif ($roll==1): ?>
 		<div class="bg_container bg_top bg_left">
-			<img src="images/CM_CropA_L_1000px.png"/>
+			<img src="bgimg/0L_CM.png"/>
 		</div>
 		<div class="bg_spacer"></div>
 		<div class="bg_container bg_btm bg_right">
-			<img src="images/CM_CropA_R_1000px.png" />
+			<img src="bgimg/0R_CM.png" />
+		</div>
+	<?php elseif ($roll==1): ?>
+		<div class="bg_container bg_btm bg_left">
+			<img src="bgimg/1L_SheepPony.png" />
+		</div>
+		<div class="bg_spacer">Art by SheepPony</div>
+		<div class="bg_container bg_btm bg_right">
+			<img src="bgimg/1R_SheepPony.png" />
 		</div>
 	<?php elseif ($roll==2): ?>
 		<div class="bg_container bg_btm bg_left">
-			<img src="images/CESS_OpenMeet_20240906_EDIT_ISOLATED-Harder.png"/>
+			<img src="bgimg/2L_Marenlicious.png"/>
+		</div>
+		<div class="bg_spacer">Art by Marenlicious &amp; Interrupter</div>
+		<div class="bg_container bg_btm bg_right">
+			<img src="bgimg/2R_Interrupter.png"/>
+		</div>
+	<?php elseif ($roll==3): ?>
+		<div class="bg_container bg_btm bg_left">
+			<img src="bgimg/3L_PaperSurgery.png"/>
+		</div>
+		<div class="bg_spacer">Art by PaperSurgery</div>
+		<div class="bg_container bg_btm bg_right">
+			<img src="bgimg/3R_PaperSurgery.png"/>
+		</div>
+	<?php elseif ($roll==4): ?>
+		<div class="bg_container bg_btm bg_left">
+			<img src="bgimg/4L_TruthOrMare.png"/>
 		</div>
 		<div class="bg_spacer">Art by TruthOrMare</div>
 		<div class="bg_container bg_btm bg_right">
-			<img src="images/Cute_Mare_Enjoyers_6_EDIT.png"/>
+			<img src="bgimg/4R_TruthOrMare.png"/>
 		</div>
 	<?php else: ?>
 		<div class="bg_container bg_top bg_left">
-			<img src="images/detail_hanmari_rot180.png"/>
+			<img src="bgimg/5L_DETAIL.png"/>
 		</div>
-		<div class="bg_spacer">Art by DETAIL &amp; Interrupter</div>
+		<div class="bg_spacer">Art by DETAIL &amp; TastiMelon</div>
 		<div class="bg_container bg_btm bg_right">
-			<img src="images/interrupter_hanmari1_NoBG.png"/>
+			<img src="bgimg/5R_TastiMelon.png"/>
 		</div>
 	<?php endif; ?>
 </div>
