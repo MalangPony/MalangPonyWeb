39ba5fd6ba011b34a1c85dea08188012f233e8c9
SheepPony <sheepponymlp@gmail.com>
2025-02-27 23:00:26 +0900

    Update video embed

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 		<span class="nobreak">다음 행사에서 만나요!</span>
 	</p>
 	
-	<iframe width="560" height="315" style="max-width:88vw;max-height:49.5vw;" src="https://www.youtube.com/embed/dwCADV2gQjk?si=exfXezg3K7_ttWlJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+	<iframe width="560" height="315" style="max-width:88vw;max-height:49.5vw;" src="https://www.youtube.com/embed/ZtCIdW_r-U8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 	
 	<a href="gallery.php" class="snsbtn snsbtn_larg malang">
 		<img src="sprites/photo_camera_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png" style="filter:drop-shadow(0 0 2px #5f4385);"/>
