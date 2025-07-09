3c03c9e8e753fec75cc8bcbd8da42b7ee03ef3a2
SheepPony <sheepponymlp@gmail.com>
2025-07-09 15:21:05 +0900

    Take web root URL from a PHP variable

--- footer.php
+++ footer.php
@@ -6,10 +6,10 @@
 <?php if ($display_sns_float===true): ?>
 <div class="bottom_floater">
 	<a href="https://x.com/MalangPony" class="snsbtn snsbtn_larg twitter">
-		<img src="/siteicons/favicon_x.png"/>공식 트위터(X)
+		<img src="<?php echo $webroot;?>/siteicons/favicon_x.png"/>공식 트위터(X)
 	</a>
 	<a class="snsbtn snsbtn_larg bluesky" style="opacity:0.2;">
-		<img src="/siteicons/favicon_bsky_white.png"/>공식 블루스카이
+		<img src="<?php echo $webroot;?>/siteicons/favicon_bsky_white.png"/>공식 블루스카이
 	</a>
 </div>
 <?php endif; ?>
@@ -27,75 +27,75 @@
 	
 	<?php if ($roll==0): ?>
 		<div class="bg_container bg_top bg_left">
-			<img src="/bgimg/0L_CM.png"/>
+			<img src="<?php echo $webroot;?>/bgimg/0L_CM.png"/>
 		</div>
 		<div class="bg_spacer"></div>
 		<div class="bg_container bg_btm bg_right">
-			<img src="/bgimg/0R_CM.png" />
+			<img src="<?php echo $webroot;?>/bgimg/0R_CM.png" />
 		</div>
 	<?php elseif ($roll==1): ?>
 		<div class="bg_container bg_btm bg_left">
-			<img src="/bgimg/1L_SheepPony.png" />
+			<img src="<?php echo $webroot;?>/bgimg/1L_SheepPony.png" />
 		</div>
 		<div class="bg_spacer">Art by SheepPony</div>
 		<div class="bg_container bg_btm bg_right">
-			<img src="/bgimg/1R_SheepPony.png" />
+			<img src="<?php echo $webroot;?>/bgimg/1R_SheepPony.png" />
 		</div>
 	<?php elseif ($roll==2): ?>
 		<div class="bg_container bg_btm bg_left">
-			<img src="/bgimg/2L_Marenlicious.png"/>
+			<img src="<?php echo $webroot;?>/bgimg/2L_Marenlicious.png"/>
 		</div>
 		<div class="bg_spacer">Art by Marenlicious &amp; Interrupter</div>
 		<div class="bg_container bg_btm bg_right">
-			<img src="/bgimg/2R_Interrupter.png"/>
+			<img src="<?php echo $webroot;?>/bgimg/2R_Interrupter.png"/>
 		</div>
 	<?php elseif ($roll==3): ?>
 		<div class="bg_container bg_btm bg_left">
-			<img src="/bgimg/3L_PaperSurgery.png"/>
+			<img src="<?php echo $webroot;?>/bgimg/3L_PaperSurgery.png"/>
 		</div>
 		<div class="bg_spacer">Art by PaperSurgery</div>
 		<div class="bg_container bg_btm bg_right">
-			<img src="/bgimg/3R_PaperSurgery.png"/>
+			<img src="<?php echo $webroot;?>/bgimg/3R_PaperSurgery.png"/>
 		</div>
 	<?php elseif ($roll==4): ?>
 		<div class="bg_container bg_btm bg_left">
-			<img src="/bgimg/4L_TruthOrMare.png"/>
+			<img src="<?php echo $webroot;?>/bgimg/4L_TruthOrMare.png"/>
 		</div>
 		<div class="bg_spacer">Art by TruthOrMare</div>
 		<div class="bg_container bg_btm bg_right">
-			<img src="/bgimg/4R_TruthOrMare.png"/>
+			<img src="<?php echo $webroot;?>/bgimg/4R_TruthOrMare.png"/>
 		</div>
 	<?php elseif ($roll==5): ?>
 		<div class="bg_container bg_top bg_left">
-			<img src="/bgimg/5L_DETAIL.png"/>
+			<img src="<?php echo $webroot;?>/bgimg/5L_DETAIL.png"/>
 		</div>
 		<div class="bg_spacer">Art by DETAIL &amp; TastiMelon</div>
 		<div class="bg_container bg_btm bg_right">
-			<img src="/bgimg/5R_TastiMelon.png"/>
+			<img src="<?php echo $webroot;?>/bgimg/5R_TastiMelon.png"/>
 		</div>
 	<?php elseif ($roll==6): ?>
 		<div class="bg_container bg_btm bg_left">
-			<img src="/bgimg/6L_Ruto.png"/>
+			<img src="<?php echo $webroot;?>/bgimg/6L_Ruto.png"/>
 		</div>
 		<div class="bg_spacer">Art by Ruto &amp; Rezina</div>
 		<div class="bg_container bg_btm bg_right">
-			<img src="/bgimg/6R_Rezina.png"/>
+			<img src="<?php echo $webroot;?>/bgimg/6R_Rezina.png"/>
 		</div>
 	<?php elseif ($roll==7): ?>
 		<div class="bg_container bg_btm bg_left">
-			<img src="/bgimg/7L_Interrupter.png"/>
+			<img src="<?php echo $webroot;?>/bgimg/7L_Interrupter.png"/>
 		</div>
 		<div class="bg_spacer">Art by Interrupter</div>
 		<div class="bg_container bg_btm bg_right">
-			<img src="/bgimg/7R_Interrupter.png"/>
+			<img src="<?php echo $webroot;?>/bgimg/7R_Interrupter.png"/>
 		</div>
 	<?php elseif ($roll==8): ?>
 		<div class="bg_container bg_btm bg_left">
-			<img src="/bgimg/8L_JullyPark.png"/>
+			<img src="<?php echo $webroot;?>/bgimg/8L_JullyPark.png"/>
 		</div>
 		<div class="bg_spacer">Art by Jully-Park</div>
 		<div class="bg_container bg_btm bg_right">
-			<img src="/bgimg/8R_JullyPark.png"/>
+			<img src="<?php echo $webroot;?>/bgimg/8R_JullyPark.png"/>
 		</div>
 	<?php else: ?>
 		
--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<?php include 'environment.php'; ?>
 <!--
 Website code by SheepPony
 Sorry for the spaghetti.
@@ -13,8 +14,8 @@ Sorry for the spaghetti.
 <?php if ($pagename=="index"): ?>
 	<meta property="og:title" content="말랑포니!" />
 	<meta property="og:type" content="website" />
-	<meta property="og:url" content="http://malangpony.com" />
-	<meta property="og:image" content="http://malangpony.com/images/MPN_Twtr_Header_var1_VECTORIZE_rev6_NoBGCrop_72.png" />
+	<meta property="og:url" content="<?php echo $webroot;?>" />
+	<meta property="og:image" content="<?php echo $webroot;?>/images/MPN_Twtr_Header_var1_VECTORIZE_rev6_NoBGCrop_72.png" />
 	<meta property="og:description" content="2025년 2월 15일 개최되는 한국의 마이리틀포니 행사입니다!" />
 <?php endif; ?>
 <!--<meta name="viewport" content="width=480" />-->
@@ -22,7 +23,7 @@ Sorry for the spaghetti.
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&family=Noto+Sans+Mono:wght@100..900&display=swap" rel="stylesheet">
-<link href="/styles.css" rel="stylesheet" />
+<link href="<?php echo $webroot;?>/styles.css" rel="stylesheet" />
 <?php if ($vertical_fit): ?>
 <style>
 html{height:100%;}
@@ -35,19 +36,19 @@ body{height:100%;}
 <div id="content">
 	<div style="height:10px;"></div>
 	<?php if (!($skip_header===true)): ?>
-	<a href="/"><img src="/images/MPN_Twtr_Header_var1_VECTORIZE_rev6_SS-NoBGCrop_72.png" id="headerimage" width="600px" height="212px"></a>
+	<a href="<?php echo $webroot;?>/"><img src="<?php echo $webroot;?>/images/MPN_Twtr_Header_var1_VECTORIZE_rev6_SS-NoBGCrop_72.png" id="headerimage" width="600px" height="212px"></a>
 	<?php endif; ?>
 	<div class="linkbar">
-		<a href="/">
+		<a href="<?php echo $webroot;?>/">
 			<div class="linkbar_btn <?php if ($pagename=='index') echo 'linkbar_selected'; ?>" style="padding-left:2px;padding-right:2px;">
-				<img src="/sprites/home_24dp_FFFFFF_FILL1_wght400_GRAD0_opsz24_EXPAND_GLOW++.png" 
+				<img src="<?php echo $webroot;?>/sprites/home_24dp_FFFFFF_FILL1_wght400_GRAD0_opsz24_EXPAND_GLOW++.png" 
 					width="48px" height="48px" class="selected_icon" />
-				<img src="/sprites/home_24dp_A485CE_FILL1_wght400_GRAD0_opsz24_EXPANDED.png" 
+				<img src="<?php echo $webroot;?>/sprites/home_24dp_A485CE_FILL1_wght400_GRAD0_opsz24_EXPANDED.png" 
 					width="48px" height="48px" class="unselected_icon" />
 			</div>
 		</a>
 		
-		<a href="/about.php">
+		<a href="<?php echo $webroot;?>/about.php">
 			<div class="linkbar_btn linkbar_red <?php if ($pagename=='about') echo 'linkbar_selected'; ?>">
 			소개
 			</div>
@@ -58,32 +59,32 @@ body{height:100%;}
 			소식
 			</div>
 		</a>-->
-		<a href="/register.php">
+		<a href="<?php echo $webroot;?>/register.php">
 			<div class="linkbar_btn linkbar_inactive <?php if ($pagename=='register') echo 'linkbar_selected'; ?>">
 			사전등록
 			</div>
 		</a>
 		
-		<a href="/rules.php">
+		<a href="<?php echo $webroot;?>/rules.php">
 			<div class="linkbar_btn linkbar_blue <?php if ($pagename=='rules') echo 'linkbar_selected'; ?>">
 			규칙
 			</div>
 		</a>
-		<a href="/timetable.php">
+		<a href="<?php echo $webroot;?>/timetable.php">
 			<div class="linkbar_btn linkbar_red <?php if ($pagename=='timetable') echo 'linkbar_selected'; ?>">
 			시간표
 			</div>
 		</a>
-		<a href="/staff.php">
+		<a href="<?php echo $webroot;?>/staff.php">
 			<div class="linkbar_btn linkbar_purple <?php if ($pagename=='staff') echo 'linkbar_selected'; ?>">
 			크레딧
 			</div>
 		</a>
-		<a href="/oc.php">
+		<a href="<?php echo $webroot;?>/oc.php">
 			<div class="linkbar_btn <?php if ($pagename=='oc') echo 'linkbar_selected'; ?>" style="padding-left:2px;padding-right:2px;">
-				<img src="/sprites/Hanmari_CM_rev1_80px_Expand128px_Glow++.png" 
+				<img src="<?php echo $webroot;?>/sprites/Hanmari_CM_rev1_80px_Expand128px_Glow++.png" 
 					width="48px" height="48px" class="selected_icon" />
-				<img src="/sprites/Hanmari_CM_rev1_80px_Expand128px.png" 
+				<img src="<?php echo $webroot;?>/sprites/Hanmari_CM_rev1_80px_Expand128px.png" 
 					width="48px" height="48px" class="unselected_icon" />
 			</div>
 		</a>
