a2ce6f254d7963df222db18d1a2811be5fdf9a59
SheepPony <sheepponymlp@gmail.com>
2025-08-30 14:03:09 +0900

    Improve main page

--- index.php
+++ index.php
@@ -6,29 +6,63 @@
 <?php include 'header.php'; ?>
 
 
-	<p class="introtext" style="margin-top:24px;margin-bottom:24px;">
+	<p class="introtext" style="margin-top:24px;">
 		<span style="font-family:NPS;">
 		다음 행사에서 만나요!
 		</span>
 	</p>
 	
 	
-	<a href="https://malangpony.com/old/post25/gallery" class="snsbtn snsbtn_larg malang_old">
-		<img src="<?php echo $webroot;?>/sprites/photo_camera_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png" style="filter:drop-shadow(0 0 2px #5f4385);"/>
-		말랑포니 1회 사진 갤러리
-	</a>
 	
+	
+	<div class="centerhead">1회 말랑포니 2025.02.15</div>
+
+	<!--
 	<a href="https://youtu.be/ZtCIdW_r-U8" class="snsbtn snsbtn_larg youtube">
 		<img src="<?php echo $webroot;?>/siteicons/YT-Logo.png"/>
 		말랑포니 1회 리캡 영상
+	</a>-->
+	<iframe width="560" height="315" style="max-width:88vw;max-height:49.5vw;" 
+	src="https://www.youtube.com/embed/ZtCIdW_r-U8" title="YouTube video player" frameborder="0" 
+	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+	referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+	<a href="https://malangpony.com/old/post25/gallery" class="snsbtn snsbtn_larg malang_old" style="margin-top:-8px;">
+		<img src="<?php echo $webroot;?>/sprites/photo_camera_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png" style="filter:drop-shadow(0 0 2px #5f4385);"/>
+		1회 말랑포니 사진 갤러리
 	</a>
-	
+
+	<div class="centerhead">말랑 리틀 포니 2025.08.23</div>
 	<a href="gallery" class="snsbtn snsbtn_larg malang">
 		<img src="<?php echo $webroot;?>/sprites/photo_camera_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png" style="filter:drop-shadow(0 0 2px #2F4575);"/>
 		말랑리틀포니 사진 갤러리
 	</a>
+	<div class="centerhead">공식 SNS</div>
+	<div class="flexrow mobile-pivot-early" style="">
+		<a href="https://x.com/MalangPony" class="snsbtn snsbtn_larg twitter">
+			<img src="<?php echo $webroot;?>/siteicons/favicon_x.png"/>공식 트위터(X)
+		</a>
+		<a href="https://bsky.app/profile/malangpony.bsky.social" class="snsbtn snsbtn_larg bluesky">
+			<img src="<?php echo $webroot;?>/siteicons/favicon_bsky_white.png"/>공식 블루스카이
+		</a>
+	</div>
+	<!--
+	<div style="height:200px;"></div>
+	<iframe width="560" height="315" style="max-width:88vw;max-height:49.5vw;" 
+	src="https://www.youtube.com/embed/ZtCIdW_r-U8" title="YouTube video player" frameborder="0" 
+	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+	referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 	
-	
+	<div class="flexrow flex-wrapping">
+		<a href="https://malangpony.com/old/post25/gallery" class="snsbtn snsbtn_larg malang_old">
+			<img src="<?php echo $webroot;?>/sprites/photo_camera_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png" style="filter:drop-shadow(0 0 2px #5f4385);"/>
+			말랑포니 1회 사진 갤러리
+		</a>
+		
+		<a href="gallery" class="snsbtn snsbtn_larg malang">
+			<img src="<?php echo $webroot;?>/sprites/photo_camera_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png" style="filter:drop-shadow(0 0 2px #2F4575);"/>
+			말랑리틀포니 사진 갤러리
+		</a>
+	</div>
 	
 	<div class="flexrow mobile-pivot-early" style="">
 		<a href="https://x.com/MalangPony" class="snsbtn snsbtn_larg twitter">
@@ -38,7 +72,7 @@
 			<img src="<?php echo $webroot;?>/siteicons/favicon_bsky_white.png"/>공식 블루스카이
 		</a>
 	</div>
-		
+	-->
 	
 	
 	<div class="normaltext" style="font-size:0.8em; text-align:center; margin-top:24px;">
